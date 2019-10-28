11f40953da7414af1dc6b72e721898d45f0d6a6f
Bonfim Justino <bonfim1999@gmail.com>
2019-10-28 00:39:17 -0300

    Sorteando usuarios cadastrados

--- sorteado/models.py
+++ sorteado/models.py
@@ -20,11 +20,15 @@ class Usuario(models.Model):
         ('40', '40'),
         ('41', '41'),
     )
-
+    SEXO = (
+        ('Masculino', 'M'),
+        ('Feminino', 'F')
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     num_calcado = models.CharField('Número do calçado', max_length=2, choices=TAMANHO)
     tamanho_saia = models.IntegerField('Tamanho da saia (Caso seja uma mulher)', blank=True, null=True)
     tamanho_blusa = models.CharField('Tamanho da camisa ou blusa', max_length=2, blank=True, null=True, choices=CHOICES)
+    sexo = models.CharField('Sexo', max_length=100, choices=SEXO)
     amigo_secreto = models.OneToOneField(User, null=True, blank=True, on_delete=models.PROTECT, related_name='user')    
 
     def __str__(self):
--- sorteado/views.py
+++ sorteado/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render
 from .models import Usuario
 from django.contrib.auth.models import User
+from django.contrib import messages
 import random
 # Create your views here.
 
 def sorteado(request):    
+    escolhido = None
+    
     if request.POST:        
-        escolhido = random.choice(User.objects.all())                                            
+        #TODO
+        #Criar recomendações
         
-        print(escolhido)   
+        users = User.objects.all().exclude(username=request.user)
+        amigos = []
+        usuario_requisicao = request.user.usuario_set.get()
+        
+        if request.user.usuario_set.first().amigo_secreto is None:
+
+            for user in Usuario.objects.all():
+                if user.amigo_secreto:
+                    amigos.append(user.amigo_secreto)#pega todos amigos selecionados
+            
+            print('amigos %s' %amigos)
+            print('todos %s' %users)
+            for amigo in amigos:
+                users = users.exclude(username=amigo)
+            print('Lista de quem sobrou: %s' % users)
+            try:
+                #print(users)
+                escolhido = random.choice(users)
+                print(type(escolhido))
+            except IndexError: 
+                messages.error(request, 'Não existe nenhum amigo que possa sortear no momento.')
+            finally:
+                    user = Usuario.objects.get(user=request.user)
+                    user.amigo_secreto = escolhido
+                    print(user.amigo_secreto)
+                    user.save()
+                    print('gravou') 
+        else:
+            escolhido = usuario_requisicao.amigo_secreto                               
      
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'amigo': escolhido})
